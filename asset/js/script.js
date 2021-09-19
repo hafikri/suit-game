cd40d4961d4044bc08354946f6654ccb8faf1b39
hafikri <hafikri@gmail.com>
2021-09-19 14:51:39 +0700

    menambahkan beberapa revisi

--- asset/js/script.js
+++ asset/js/script.js
@@ -22,11 +22,7 @@ pilihImgPlayer.forEach(function (i) {
     i.classList.toggle("active");
 
     const imgComputer = document.querySelector(`.comp .${pilComp}`);
-    // console.log(imgComputer);
     imgComputer.classList.toggle("active");
-
-    //   const imgCompSelect = imgComputer.querySelector(".batu");
-    //   console.log(imgCompSelect);
 
     const info = document.querySelector(".info");
     info.classList.toggle("menang");
@@ -38,49 +34,3 @@ const refresh = document.querySelector(".reload");
 refresh.addEventListener("click", function () {
   window.location.reload();
 });
-// console.log(refresh);
-
-// const pBatu = document.querySelector(".player .batu");
-// pBatu.addEventListener("click", function () {
-//   const pilComp = getPilComp();
-//   const pilPlayer = pBatu.className;
-//   const hasil = getHasil(pilComp, pilPlayer);
-
-//   const imgComputer = document.querySelector(".comp img");
-//   //   const imgCompSelect = imgComputer.querySelector(".batu");
-//   //   console.log(imgCompSelect);
-
-//   const info = document.querySelector(".info");
-//   info.classList.toggle("menang");
-//   info.innerHTML = hasil;
-// });
-
-// const pKertas = document.querySelector(".player .kertas");
-// pKertas.addEventListener("click", function () {
-//   const pilComp = getPilComp();
-//   const pilPlayer = pKertas.className;
-//   const hasil = getHasil(pilComp, pilPlayer);
-
-//   const imgComputer = document.querySelector(".comp img");
-//   //   const imgCompSelect = imgComputer.querySelector(".batu");
-//   //   console.log(imgCompSelect);
-
-//   const info = document.querySelector(".info");
-//   info.classList.toggle("menang");
-//   info.innerHTML = hasil;
-// });
-
-// const pGunting = document.querySelector(".player .gunting");
-// pGunting.addEventListener("click", function () {
-//   const pilComp = getPilComp();
-//   const pilPlayer = pGunting.className;
-//   const hasil = getHasil(pilComp, pilPlayer);
-
-//   const imgComputer = document.querySelector(".comp img");
-//   //   const imgCompSelect = imgComputer.querySelector(".batu");
-//   //   console.log(imgCompSelect);
-
-//   const info = document.querySelector(".info");
-//   info.classList.toggle("menang");
-//   info.innerHTML = hasil;
-// });
