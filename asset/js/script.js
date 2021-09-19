7a892a3cd8995359947c5e7b3269ebbb89295a83
hafikri <hafikri@gmail.com>
2021-09-19 11:01:20 +0700

    penambahan code function select comp auto

--- asset/js/script.js
+++ asset/js/script.js
@@ -13,47 +13,74 @@ function getHasil(comp, player) {
   if (player == "gunting") return comp == "kertas" ? "PLAYER 1 WIN" : "COMP WIN";
 }
 
-const pBatu = document.querySelector(".player .batu");
-pBatu.addEventListener("click", function () {
-  const pilComp = getPilComp();
-  const pilPlayer = pBatu.className;
-  const hasil = getHasil(pilComp, pilPlayer);
+const pilihImgPlayer = document.querySelectorAll(".player img");
+pilihImgPlayer.forEach(function (i) {
+  i.addEventListener("click", function () {
+    const pilComp = getPilComp();
+    const pilPlayer = i.className;
+    const hasil = getHasil(pilComp, pilPlayer);
+    i.classList.toggle("active");
 
-  const imgComputer = document.querySelector(".comp img");
-  //   const imgCompSelect = imgComputer.querySelector(".batu");
-  //   console.log(imgCompSelect);
+    const imgComputer = document.querySelector(`.comp .${pilComp}`);
+    // console.log(imgComputer);
+    imgComputer.classList.toggle("active");
 
-  const info = document.querySelector(".info");
-  info.classList.toggle("menang");
-  info.innerHTML = hasil;
+    //   const imgCompSelect = imgComputer.querySelector(".batu");
+    //   console.log(imgCompSelect);
+
+    const info = document.querySelector(".info");
+    info.classList.toggle("menang");
+    info.innerHTML = hasil;
+  });
 });
 
-const pKertas = document.querySelector(".player .kertas");
-pKertas.addEventListener("click", function () {
-  const pilComp = getPilComp();
-  const pilPlayer = pKertas.className;
-  const hasil = getHasil(pilComp, pilPlayer);
-
-  const imgComputer = document.querySelector(".comp img");
-  //   const imgCompSelect = imgComputer.querySelector(".batu");
-  //   console.log(imgCompSelect);
-
-  const info = document.querySelector(".info");
-  info.classList.toggle("menang");
-  info.innerHTML = hasil;
+const refresh = document.querySelector(".reload");
+refresh.addEventListener("click", function () {
+  window.location.reload();
 });
+// console.log(refresh);
 
-const pGunting = document.querySelector(".player .gunting");
-pGunting.addEventListener("click", function () {
-  const pilComp = getPilComp();
-  const pilPlayer = pGunting.className;
-  const hasil = getHasil(pilComp, pilPlayer);
+// const pBatu = document.querySelector(".player .batu");
+// pBatu.addEventListener("click", function () {
+//   const pilComp = getPilComp();
+//   const pilPlayer = pBatu.className;
+//   const hasil = getHasil(pilComp, pilPlayer);
 
-  const imgComputer = document.querySelector(".comp img");
-  //   const imgCompSelect = imgComputer.querySelector(".batu");
-  //   console.log(imgCompSelect);
+//   const imgComputer = document.querySelector(".comp img");
+//   //   const imgCompSelect = imgComputer.querySelector(".batu");
+//   //   console.log(imgCompSelect);
 
-  const info = document.querySelector(".info");
-  info.classList.toggle("menang");
-  info.innerHTML = hasil;
-});
+//   const info = document.querySelector(".info");
+//   info.classList.toggle("menang");
+//   info.innerHTML = hasil;
+// });
+
+// const pKertas = document.querySelector(".player .kertas");
+// pKertas.addEventListener("click", function () {
+//   const pilComp = getPilComp();
+//   const pilPlayer = pKertas.className;
+//   const hasil = getHasil(pilComp, pilPlayer);
+
+//   const imgComputer = document.querySelector(".comp img");
+//   //   const imgCompSelect = imgComputer.querySelector(".batu");
+//   //   console.log(imgCompSelect);
+
+//   const info = document.querySelector(".info");
+//   info.classList.toggle("menang");
+//   info.innerHTML = hasil;
+// });
+
+// const pGunting = document.querySelector(".player .gunting");
+// pGunting.addEventListener("click", function () {
+//   const pilComp = getPilComp();
+//   const pilPlayer = pGunting.className;
+//   const hasil = getHasil(pilComp, pilPlayer);
+
+//   const imgComputer = document.querySelector(".comp img");
+//   //   const imgCompSelect = imgComputer.querySelector(".batu");
+//   //   console.log(imgCompSelect);
+
+//   const info = document.querySelector(".info");
+//   info.classList.toggle("menang");
+//   info.innerHTML = hasil;
+// });
